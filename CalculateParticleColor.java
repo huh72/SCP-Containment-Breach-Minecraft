9d2e4fa2075cba94e2ee72aed13e8ae1e370c6d8
Huh <petrimamanata@gmail.com>
2026-05-20 17:58:36 +0300

    Added Java script, which calculates color (float) for particles

---
+++ CalculateParticleColor.java
@@ -0,0 +1,19 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        double x = 248;
+        double y = 255;
+        double z;
+
+        z = x / y;
+
+        //String result = String.valueOf(z).replace('.', ',');
+
+        System.out.println( z );
+
+    }
+}
